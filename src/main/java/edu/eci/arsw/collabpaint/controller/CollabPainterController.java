de0167290f6eefbbe356efdefc89ddc840eb43ba
unknown <tomas.suarez@labinfo.is.escuelaing.edu.co>
2024-10-17 08:50:19 -0500

    PARTE 1

---
+++ src/main/java/edu/eci/arsw/collabpaint/controller/CollabPainterController.java
@@ -0,0 +1,20 @@
+package edu.eci.arsw.collabpaint.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import edu.eci.arsw.collabpaint.model.Point;
+
+
+@Controller
+public class CollabPainterController {
+
+    @MessageMapping("/newpoint")
+    @SendTo("/topic/newpoint")
+    public Point broadcastPoint(Point point) {
+        //System.out.println("Received point: " + point);
+        return point;
+    }
+
+}
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -36,8 +36,14 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
                 
+                var theObject = JSON.parse(eventbody.body);
+
+                var x = theObject.x;
+                var y = theObject.y;
+                
+                alert('Punto recibido:\nX: ' + x + '\nY: ' + y  );
                 
             });
         });
@@ -61,6 +67,10 @@ var app = (function () {
             addPointToCanvas(pt);
 
             //publicar el evento
+            //creando un objeto literal
+            stompClient.send("/topic/newpoint", {}, JSON.stringify({x:px,y:py}));
+            //enviando un objeto creado a partir de una clase
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt)); 
         },
 
         disconnect: function () {
